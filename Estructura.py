3768c303f24e045eeaecd0ea576f5b8382a8bbb5
Gonzalo Ramiro Irigoyen <irigoyengon@hotmail.com>
2024-04-10 23:22:40 -0300

    Creacion de archivos y modulos, y resolucion del punto 1

---
+++ Estructura.py
@@ -0,0 +1,8 @@
+def crear_estructura(names, goals, goals_avoided, assists):
+    caracteres_especiales=[" ","'",'\n']
+    for car in caracteres_especiales:
+        names=names.replace(car,"")
+    names=names.replace("\n",",")
+    names=names.split(",")
+    return {"Nombres":names, "Goles":goals,"Evitados":goals_avoided, "Asistencias":assists}
+    
